0376760e88413a3f127b76ef1e654e1d958d47fa
Kostas <diamantopoulos.kostas@hotmail.com>
2024-05-05 21:20:57 +0100

    locations added

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -1,70 +1,82 @@
 import { MapPinIcon } from '@heroicons/react/24/outline'
-  
-  const actions = [
-    {
-      title: 'London',
-      icon: MapPinIcon
-    },
-    {
-      title: 'London',
-      icon: MapPinIcon
-    },
-    {
-      title:'Manchester',
-      icon: MapPinIcon
-    },
-    {
-      title: 'Liverpool',
-      icon: MapPinIcon
-    },
-    {
-      title: 'Brighton',
-      icon: MapPinIcon
-    },
-    {
-      title: 'Birmingham',
-      icon: MapPinIcon
-    },
-  ]
-  
-  function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
-  }
-  
-  export default function Stores() {
-    return (
-      <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
-        {actions.map((action, actionIdx) => (
-          <div
-            key={action.title}
-            className={classNames(
-              actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
-              actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
-              actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '',
-              actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
-              'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
-            )}
-          >
-            <div>
-              <span
-                className={classNames(
-                  'inline-flex rounded-lg p-3 ring-4 ring-white'
-                )}
-              >
-                <action.icon className="h-6 w-6" aria-hidden="true" />
-              </span>
-            </div>
-            <div className="mt-8">
-              <h3 className="text-base font-semibold leading-6 text-gray-900">
-              </h3>
-              <p className="mt-2 text-sm text-gray-500">
-                Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et
-                quo et molestiae.
-              </p>
-            </div>
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const actions = [
+  {
+    title: 'London',
+    icon: MapPinIcon,
+    description: '130 Thessaly Rd, Nine Elms, London SW8 5EJ, United Kingdom.'
+  },
+  {
+    title: 'Manchester',
+    icon: MapPinIcon,
+    description: 'University of, Oxford Rd, Manchester M13 9PL, United Kingdom.'
+  },
+  {
+    title: 'Liverpool',
+    icon: MapPinIcon,
+    description: 'University of, Oxford Rd, Liverpool M13 9PL, United Kingdom.'
+  },
+  {
+    title: 'Brighton',
+    icon: MapPinIcon,
+    description: 'Royal Pavilion Gardens, Brighton and Hove, Brighton BN1 1EE, United Kingdom.'
+  },
+  {
+    title: 'Birmingham',
+    icon: MapPinIcon,
+    description: 'Royal Pavilion Gardens, Brighton and Hove, Birmingham BN1 1EE, United Kingdom.'
+  },
+  {
+    title: 'Devon',
+    icon: MapPinIcon,
+    description: '130 Thessaly Rd, Nine Elms, Devon SW8 5EJ, United Kingdom.'
+  },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Stores() {
+  return (
+    <>
+    <Navbar />
+
+    <div className="divide-y text-amber-500 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+      {actions.map((action, actionIdx) => (
+        <div
+          key={action.title}
+          className={classNames(
+            actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+            actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
+            actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '',
+            actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
+            'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
+          )}
+        >
+          <div className="flex items-center">
+            <span
+              className={classNames(
+                'inline-flex rounded-lg p-3 ring-4 ring-white'
+              )}
+            >
+              <action.icon className="h-6 w-6" aria-hidden="true" />
+            </span>
+            <h3 className="ml-4 text-base font-semibold leading-6 text-gray-900">
+              {action.title}
+            </h3>
           </div>
-        ))}
-      </div>
-    )
-  }
-  
+          <div className="mt-4">
+            <p className="text-sm text-gray-500">
+              {action.description}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+    <Footer />
+    </>
+  )
+}
