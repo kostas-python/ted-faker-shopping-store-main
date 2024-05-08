b79c0b4e4bdd7a3877b9f4e3ab4ca6a57ea66756
Kostas <diamantopoulos.kostas@hotmail.com>
2024-05-08 22:08:25 +0100

    added reviews

--- src/app/products/blackbasictee/page.tsx
+++ src/app/products/blackbasictee/page.tsx
@@ -272,6 +272,29 @@ const reviews = {
       date: 'May 16, 2021',
       datetime: '2021-01-06',
     },
+    {
+      id: 2,
+      title: "Very comfy and looks the part",
+      rating: 5,
+      content: `
+        <p>After a quick chat with customer support, I had a good feeling about this shirt and ordered three of them.</p>
+        <p>Less than 48 hours later, my delivery arrived. I haven't worn anything else since that day! These shirts are so comfortable, yet look classy enough that I can wear them at work or even some formal events. Winning!</p>
+      `,
+      author: 'Jackie H',
+      date: 'April 6, 2021',
+      datetime: '2021-01-06',
+    },
+    {
+      id: 3,
+      title: "The last shirts I may ever need",
+      rating: 4,
+      content: `
+        <p>I bought two of those comfy cotton shirts, and let me tell you: they're amazing! I have been wearing them almost every day. Even after a dozen of washes, that still looks and feel good as new. Will definitely order a few more... If I ever need to!</p>
+      `,
+      author: 'Laura G',
+      date: 'February 28, 2021',
+      datetime: '2021-01-06',
+    },
     // More reviews...
   ],
 }
