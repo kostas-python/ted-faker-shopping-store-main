eca04df18626397674822beec8f5c54dabfa4e77
Kostas <diamantopoulos.kostas@hotmail.com>
2024-05-19 12:16:00 +0100

    bags component updates

--- src/app/pages/bags/page.tsx
+++ src/app/pages/bags/page.tsx
@@ -77,7 +77,7 @@ const products = [
         <div className="mx-auto max-w-7xl overflow-hidden px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
             {products.map((product) => (
-              <a key={product.id} href={product.href} className="group text-sm">
+              <a key={product.id} href={product.href} className={`group text-sm animate__animated animate__zoomIn`}>
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
                   <img
                     src={product.imageSrc}
