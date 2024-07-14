79120c0ddc288a3cd8931fbc24f4086910676ed7
Kostas <diamantopoulos.kostas@hotmail.com>
2024-07-14 10:47:56 +0100

    created entities folder

---
+++ src/app/Entities/Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.app.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        
+        public long Price { get; set; }
+
+        public int ReviewCount { get; set; }
+
+        public int Rating { get; set; }
+        
+        public string Imagesrc { get; set; }
+
+        public int QuantityInStock { get; set; }
+    }
+}
